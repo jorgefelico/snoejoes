832d7385198ead92418df5d4e749903ba86db161
jorgefelico@gmail.com <jorgefelico@gmail.com>
2015-05-19 09:40:04 -0400

    Main files push

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sno-E Joe's Shaved Ice</title>
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/foundation.min.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link href='http://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="fullwidth header-wrap">
+    <header class="row">
+      <div class="columns large-4">
+        <img class="logo" src="img/snoejoes-logo.png" alt="Sno-E Joe's Logo" />
+      </div>
+      <div class="columns large-8">
+        <p class="chalkboard">Shaved Ice with <span class="red">unique flavors</span> or <span class="green">create your own</span> favorite flavors. <span class="yellow">Drinks</span> and also packaged <span class="blue">ice creams</span> available.</p>
+      </div>
+    </header>
+  </div>
+
+  <div class="fullwidt flavors-wrap">
+    <div class="flavors row">
+      <h2>Many! Many! Flavors!</h2>
+
+      <div class="row unique-flavors">
+        <div class="columns large-6">
+          <p class="create-your-own">Create your own shaved ice slushy flavor by mixing any flavors of your choice!</p>
+        </div>
+        <div class="columns large-3">
+          <h3>*SUGAR FREE*</h3>
+          <ul class="sugar-free">
+            <li>Cherry</li>
+            <li>Coconut</li>
+            <li>Grape</li>
+          </ul>
+        </div>
+        <div class="columns large-3">
+          <h3>*DYE FREE*</h3>
+          <ul class="dye-free">
+            <li>Clear Cherry</li>
+            <li>Clear Strawberry</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="columns large-3">
+        <ul>
+          <li>Apricot</li>
+          <li>Bahama Mama</li>
+          <li>Banana</li>
+          <li>Black Cherry</li>
+          <li>Blackberry</li>
+          <li>Blue Bubble Gum</li>
+          <li>Blue Coconut</li>
+          <li>Blue Cotton Candy</li>
+          <li>Blue Eagle</li>
+          <li>Blue Hawaiian</li>
+          <li>Blue Raspberry</li>
+          <li>Blue Berry</li>
+          <li>Buttercream</li>
+          <li>Butterscotch</li>
+          <li>Cake Batter</li>
+          <li>Cantaloupe</li>
+          <li>Cappuccino</li>
+          <li>Cheesecake</li>
+          <li>Cherimoya</li>
+          <li>Cherry</li>
+          <li>Cherry Cola</li>
+          <li>Chocolate</li>
+          <li>Chocolate Covered Cherry</li>
+        </ul>
+      </div>
+      <div class="columns large-3">
+        <ul>
+          <li>Chocolate Covered Strawberry</li>
+          <li>Coco-Banana</li>
+          <li>Coconut</li>
+          <li>Coffee</li>
+          <li>Cola</li>
+          <li>Cream Soda</li>
+          <li>Creamy Coconut</li>
+          <li>Daiquiri</li>
+          <li>Dill</li>
+          <li>Dreamcicle</li>
+          <li>Egg Custard</li>
+          <li>Fruit Punch</li>
+          <li>Fuzzy Navel</li>
+          <li>Granny Smith Apple</li>
+          <li>Grape</li>
+          <li>Green Apple</li>
+          <li>Guava</li>
+          <li>Hawaiian</li>
+          <li>Homemade Ice Cream</li>
+          <li>Horchata</li>
+          <li>Ice Cream</li>
+          <li>Key Lime Pie</li>
+          <li>Kiwi</li>
+        </ul>
+      </div>
+      <div class="columns large-3">
+        <ul>
+          <li>Leche</li>
+          <li>Lemon</li>
+          <li>Lemon-Lime</li>
+          <li>Lime</li>
+          <li>Mango</li>
+          <li>Margarita</li>
+          <li>Mocha</li>
+          <li>Mudslide</li>
+          <li>Orange</li>
+          <li>Orange Pineapple</li>
+          <li>Orange Splash</li>
+          <li>Passion Fruit</li>
+          <li>Peach</li>
+          <li>Peaches & Cream</li>
+          <li>Piña Colada</li>
+          <li>Pineapple</li>
+          <li>Pink Bubble Gum</li>
+          <li>Pink Cotton Candy</li>
+          <li>Pink Lemon Sour</li>
+          <li>Pink Lemonade</li>
+          <li>Polar Punch</li>
+          <li>Red Apple</li>
+          <li>Red Hot</li>
+        </ul>
+      </div>
+      <div class="columns large-3">
+        <ul>
+          <li>Red Raspberry</li>
+          <li>Red Velvet Cake</li>
+          <li>Rock & Roll</li>
+          <li>Root Beer</li>
+          <li>Ruby Red Grapefruit</li>
+          <li>Sour Apple Silver Fox</li>
+          <li>Sour Cherry</li>
+          <li>Sour Grape</li>
+          <li>Spearmint</li>
+          <li>Strawberry</li>
+          <li>Strawberry Cheesecake</li>
+          <li>Strawberry Daiquiri</li>
+          <li>Strawberry-Banana</li>
+          <li>Strawberry-Kiwi</li>
+          <li>Tangerine</li>
+          <li>Tiger's Blood</li>
+          <li>Tutti Fruiti</li>
+          <li>Vanilla</li>
+          <li>Vanilla Cola</li>
+          <li>Watermelon</li>
+          <li>Wedding Cake</li>
+          <li>Wild Cherry</li>
+          <li>Wild Strawberry</li>
+          <li>Wine Cooler</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+include('./partials/footer.html');
+?>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+$(document).foundation();
+</script>
+</body>
+</html>
