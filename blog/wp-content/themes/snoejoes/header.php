ad61f61366ca3fb00943f764175bbc6d2fc91743
jorgefelico@gmail.com <jorgefelico@gmail.com>
2015-05-25 16:43:07 -0400

    Fixed blog header not correctly linking css file

--- blog/wp-content/themes/snoejoes/header.php
+++ blog/wp-content/themes/snoejoes/header.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/foundation.min.css">
-  <link rel="stylesheet" href="../css/foundation-icons/foundation-icons.css" />
+  <link rel="stylesheet" href="../../css/foundation-icons/foundation-icons.css" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
   <?php wp_head(); ?>
 </head>
