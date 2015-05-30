c9906c376a3b0edc24b16a22ad1e4df8adb640a5
jorgefelico@gmail.com <jorgefelico@gmail.com>
2015-05-29 22:43:11 -0400

    added social sharing modal to footer

--- partials/footer.php
+++ partials/footer.php
@@ -88,7 +88,7 @@ wow.init();
 <div id="sharing" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
   <h2 id="modalTitle">Go Ahead, Give us a like!</h2>
   <p class="lead">Share your ice cream and snow cone experience with your friends.</p>
-  <ul>
+  <ul class="small-block-grid-8">
 <li><div class="fb-like" data-href="http://www.snoejoes.com" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div></li>
 <li><!-- Place this tag in your head or just before your close body tag. -->
 <script src="https://apis.google.com/js/platform.js" async defer></script>
